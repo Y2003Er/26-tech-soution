c023d381c774cae25834c1ec889924d8428b6337
Y2003Er <yusuphhanigomba7@gmail.com>
2026-06-30 15:06:18 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const appController = require('../controllers/appController');
+const NewsletterController = require('../controllers/newsletterController');
 
 // Ukurasa wa nyumbani (Home Page)
 router.get('/', appController.index);
 
 // Ukurasa wa undani wa app (Details Page)
 router.get('/app/:slug', appController.details);
+
+// Newsletter
+router.post('/newsletter/subscribe', NewsletterController.subscribe);
 
 // Kurasa za kisheria na mawasiliano (Footer Pages)
 router.get('/about', appController.about);
