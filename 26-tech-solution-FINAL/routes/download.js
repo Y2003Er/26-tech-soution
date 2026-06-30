b61720012af23859aeb5789ef7b28f125a244115
Y2003Er <yusuphhanigomba7@gmail.com>
2026-06-30 07:59:57 +0000

    28

--- 26-tech-solution-FINAL/routes/download.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const DownloadController = require('../controllers/downloadController');
-
-router.get('/download/:slug', DownloadController.downloadPage);
-router.get('/go/:slug', DownloadController.goDownload);
-router.get('/icon/:fileId', DownloadController.getIcon);
-
-module.exports = router;
