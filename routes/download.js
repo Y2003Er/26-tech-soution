23f04c74b7fe727debb1dcbf4fe41e201f5b0567
Y2003Er <yusuphhanigomba7@gmail.com>
2026-06-30 14:33:45 -0700

    Update download.js

--- routes/download.js
+++ routes/download.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const DownloadController = require('../controllers/downloadController');
 
 router.get('/download/:slug', DownloadController.downloadPage);
-router.get('/go/:slug', DownloadController.goDownload);
+router.post('/download/:slug/prepare', DownloadController.prepareDownload);
+
+router.get('/go/:slug/:token', DownloadController.serveDownload);
+router.get('/go/:slug', DownloadController.legacyRedirect);
+
 router.get('/icon/:fileId', DownloadController.getIcon);
 
 module.exports = router;
